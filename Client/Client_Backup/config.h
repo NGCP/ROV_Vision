182b0be10a20db174f8651d1696533c8c21d52dd
Jeremy Passehl <jeremy.passehl67@gmail.com>
2017-09-19 06:58:12 -0700

    Client

---
+++ Client/Client_Backup/config.h
@@ -0,0 +1,18 @@
+#define FRAME_HEIGHT 600
+#define FRAME_WIDTH 800
+#define FRAME_INTERVAL (1000/30)
+#define PACK_SIZE 61440 //udp pack size; note that OSX limits < 8100 bytes
+#define IMG_SIZE 921600
+#define STREAM_SIZE 614400
+#define ROWS 480
+#define COLS 640
+#define ENCODE_QUALITY 80
+
+/*
+CV_8UC3
+Type: 16 16
+Size: 921600 921600
+Total: 307200 307200
+Element Size: 3 3
+Channels: 3 3
+*/
